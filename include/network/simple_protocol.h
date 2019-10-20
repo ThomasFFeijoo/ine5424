fd4d1fd44c3e4778e1f5f35764fa363a2e5162ed
lucas <lucasjoao.lj@gmail.com>
2019-10-20 17:14:50 -0300

    WIP simple protocol with necessary things.

--- include/network/simple_protocol.h
+++ include/network/simple_protocol.h
@@ -12,42 +12,33 @@
 // TODO: what is this?
 __BEGIN_SYS
 
-class Simple_Protocol: private NIC<Ethernet>::Observer {
-
-    friend class System;
-    friend class Network_Common;
+class Simple_Protocol:
+    private NIC<Ethernet>::Observer,
+    private Concurrent_Observer<Ethernet::Buffer, Ethernet::Protocol> {
 
 public:
-    // Buffers received by the NIC, eventually linked into a list
+
     typedef Ethernet::Buffer Buffer;
-    // IP and NIC observer/d
-    typedef Data_Observer<Buffer, Protocol> Observer;
-    typedef Data_Observed<Buffer, Protocol> Observed;
-
-protected:
-
-    template<unsigned int UNIT = 0>
-    Simple_Protocol(unsigned int nic = UNIT);
 
 public:
+
+    Simple_Protocol(unsigned int nic = 0) :
+            _nic(Traits<Ethernet>::DEVICES::Get<0>::Result::get(nic))
+    {
+        _nic->attach(this, Ethernet::PROTO_SP);
+    }
+
+    const Address & address();
 
     static int send(const void * data, unsigned int size);
 
     static int receive(void * data, unsigned int size);
 
-    static const unsigned int mtu() { return MTU; }
-
-private:
-
-    void update(Ethernet::Observed * obs, const Ethernet::Protocol & prot, Buffer * buf);
-
-    static bool notify(Buffer * buf) { return _observed.notify(buf); }
+    void update(Observed *obs, const Ethernet::Protocol & prot, Buffer * buf);
 
 protected:
 
     NIC<Ethernet> * _nic;
-    Address _address;
-    static Observed _observed;
 
 };
 
