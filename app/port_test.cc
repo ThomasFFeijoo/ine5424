fc4b1fbce4d18b89096f5ee1f0cd68e7f0b3f906
Thomas <thomwe8@hotmail.com>
2019-10-26 18:07:28 -0300

    added port test succces and error

--- app/port_test.cc
+++
@@ -1,7 +0,0 @@
-using namespace EPOS;
-
-OStream cout;
-
-int main() {
-    cout << "Port Test" << endl;
-}
---
+++ app/port_test_sucess.cc
@@ -0,0 +1,34 @@
+// EPOS NIC Test Programs
+
+#include <machine/nic.h>
+#include <time.h>
+#include <network/simple_protocol.h>
+
+using namespace EPOS;
+
+OStream cout;
+
+int main()
+{
+    cout << "Port Test" << endl;
+    
+    Simple_Protocol * sp = new Simple_Protocol();
+
+    Simple_Protocol::Address self = sp->address();
+
+    cout << "  MAC: " << self << endl;
+
+    char data[22];
+    char * text = "my text to say hello\n";
+
+    if(self[5] % 2) { // sender
+        Delay (5000000);
+
+            cout << " Sending: " << text;
+            sp->send(Simple_Protocol::Address::BROADCAST, 99, text, 22);
+        
+    } else { // receiver
+           sp->receive(99, data, 22);
+           cout << "  Data: " << data;
+    }
+}
