2230a922999a0e9afcd9b7e67a157c99ada7e504
Thomas <thomwe8@hotmail.com>
2019-10-28 20:38:01 -0300

    new test just to test send using thread

---
+++ app/test_multiple.cc
@@ -0,0 +1,51 @@
+#include <machine/nic.h>
+#include <time.h>
+#include <network/simple_protocol.h>
+
+using namespace EPOS;
+
+OStream cout;
+
+Simple_Protocol * sp;
+Simple_Protocol::Address self;
+const int MTU = 1500;
+char data[MTU];
+
+int sender()
+{
+    int port = 10;
+    Simple_Protocol::Address other = self;
+    other[5]--;
+    char * text = (char*) "my text to say hello\n";
+    for(int port = 10; port < 15; port++) {
+      cout << "SENDING " << text << endl;
+      cout << "Enviado para a porta " << port << endl;
+      sp->send(other, port, text, MTU);
+    }
+}
+
+int receiver()
+{
+    for(int port = 10; port < 15; port++) {
+        sp->receive(port, data, 1500);
+        cout << "Recebido na porta: " << port << endl;
+        cout << " Data: " << data << endl;
+        
+        }
+}
+
+
+int main()
+{
+    sp = new Simple_Protocol();
+    self = sp->address();
+
+    cout << "  MAC: " << self << endl;
+
+    if(self[5] % 2) { // sender
+        Delay (5000000);
+        new Thread(&sender);
+    } else { // receiver
+        new Thread(&receiver);
+    }
+}
