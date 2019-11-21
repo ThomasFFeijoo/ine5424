9a8bc7fb542087fbea599cab0e2f492b765db1c1
lucas <lucasjoao.lj@gmail.com>
2019-11-21 17:30:08 -0300

    Use send in p4_uart_test.

--- app/p4_uart_test.cc
+++ app/p4_uart_test.cc
@@ -18,10 +18,15 @@ int main() {
 
     sp = new Simple_Protocol();
     Simple_Protocol::Address self = sp->address();
+    char * text = (char*) "my text to say hello\n";
 
     if(self[5] % 2) { // sender
         Delay(5000000);
         new Thread(&uart_thread);
+
+        Simple_Protocol::Address other = self;
+        other[5]--;
+        sp->send(other, 99, text, Traits<Simple_Protocol>::MTU);
     } else { // receiver
     }
 }
