d8be50e97c8420c2a2045ac5ccdb09362b796799
lucas <lucasjoao.lj@gmail.com>
2019-11-24 23:59:58 -0300

    More clean code.

--- src/utility/external_libs.cc
+++ src/utility/external_libs.cc
@@ -4,25 +4,6 @@
 
 __BEGIN_UTIL
 
-// src https://www.geeksforgeeks.org/square-root-of-a-number-without-using-sqrt-function/
-double Helper::square(double n, double i, double j) {
-	double mid = (i + j) / 2;
-	double mul = mid * mid;
-
-	// If mid itself is the square root,
-	// return mid
-	if ((mul == n) || (fabs(mul - n) < 0.00001))
-		return mid;
-
-	// If mul is less than n, recur second half
-	else if (mul < n)
-		return square(n, mid, j);
-
-	// Else recur first half
-	else
-		return square(n, i, mid);
-}
-
 // src https://searchcode.com/codesearch/view/10260157/
 bool Helper::isspace(unsigned char c) {
     if ( c == ' '
@@ -43,23 +24,5 @@ bool Helper::isdigit(unsigned char c) {
 
     return false;
 }
-
-// src http://www.beedub.com/Sprite093/src/lib/m/fabs.c
-/*
- *----------------------------------------------------------------------
- *
- * fabs --
- *
- *	Return the absolute value of a floating-point number
- *
- * Results:
- *	Absolute value of x.
- *
- * Side effects:
- *	None.
- *
- *----------------------------------------------------------------------
- */
-
 
 __END_UTIL
